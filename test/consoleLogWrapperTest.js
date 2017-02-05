c28fdfb6b1d587ed3f48052896e23ee2a2dd69ee
Larene Le Gassick <larenelg@thoughtworks.com>
2017-02-05 22:37:08 +1000

    use let instead of var

--- test/consoleLogWrapperTest.js
+++ test/consoleLogWrapperTest.js
@@ -1,5 +1,4 @@
 let chai = require('chai')
-let assert = chai.assert
 let expect = chai.expect
 
 let sinon = require('sinon')
@@ -19,29 +18,29 @@ describe('Console log wrapper', () => {
   })
 
   it('should log variable with filename and line no', () => {
-    var lolog = new LOLOG() 
+    let lolog = new LOLOG()
 
-    var someVar = 42 
-    var anotherVar = 'lolcat'
-    var logoutput = lolog.log(someVar, anotherVar) // NOTE THIS LINE NUMBER!
+    let someVar = 42
+    let anotherVar = 'lolcat'
+    lolog.log(someVar, anotherVar) // NOTE THIS LINE NUMBER!
 
-    expect(console.log).to.be.calledWith('{"0":42,"1":"lolcat"}, consoleLogWrapperTest.js:26')
+    expect(console.log).to.be.calledWith('{"0":42,"1":"lolcat"}, consoleLogWrapperTest.js:25')
   })
 
   it('should log the variable name and value', () => {
-    var lolog = new LOLOG()
+    let lolog = new LOLOG()
 
-    var someVar = 42 
+    let someVar = 42
     lolog.logvar(someVar)
 
     expect(console.log).to.be.calledWith('someVar is {"0":42}')
   })
 
   it('should log multiple variables names and values', () => {
-    var lolog = new LOLOG()
+    let lolog = new LOLOG()
 
-    var someVar = 42 
-    var anotherVar = { 'oh no': 'not again' }
+    let someVar = 42
+    let anotherVar = { 'oh no': 'not again' }
     lolog.logvar(someVar, anotherVar)
 
     expect(console.log).to.be.calledWith('someVar, anotherVar are {"0":42,"1":{"oh no":"not again"}}')
